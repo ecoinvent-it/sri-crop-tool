634563d6623ee1a7d846f9b41298411537b10f54
cdelerce <clementine.delerce@quantis-intl.com>
2017-11-10 15:49:23 +0100

    new visual interface for ecoinvent

--- src/main/dart/lib/toolPage/tool_page.dart
+++ src/main/dart/lib/toolPage/tool_page.dart
@@ -1,10 +1,13 @@
 library tool_page;
 
+import 'dart:html';
+
 import 'package:alcig/login/loginBox/login_box.dart';
 import 'package:alcig/login/userBox/user_box.dart';
 import 'package:alcig/notificationModal/notification_modal.dart';
 import 'package:alcig/processGenerationSteps/process_generation_steps.dart';
 import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
 
 @Component(
         selector: 'tool-page',
@@ -13,5 +16,17 @@ import 'package:angular/angular.dart';
         templateUrl: 'tool_page.html')
 class ToolPage
 {
-    ToolPage();
+    Router _router;
+
+    ToolPage(Router this._router);
+
+    void goToFaq()
+    {
+        _router.navigate(["../Faq"]);
+    }
+
+    void goToEcoinvent()
+    {
+        window.location.href = 'http://www.ecoinvent.org/';
+    }
 }
